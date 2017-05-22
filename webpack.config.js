028ec820aa305b2d5875bd99a59cf664055eb5ae
JaydenXu <jaydenxu1992@gmail.com>
2017-05-22 16:09:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:8080',
-        path.resolve(__dirname, 'app/main.js'),
-    ],
+    entry: path.resolve(__dirname, 'app/main.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'dist_bundle.js',
